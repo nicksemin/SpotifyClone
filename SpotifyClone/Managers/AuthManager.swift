17a62db6f15f34a90bcadd484fd7452ea69916a0
“”Nikolai <semin.nikolai@bk.ru>
2023-03-22 22:20:55 +0200

    Token refresh is active now

--- SpotifyClone/Managers/AuthManager.swift
+++ SpotifyClone/Managers/AuthManager.swift
@@ -14,13 +14,14 @@ final class AuthManager {
         static let clientID  = "5b65105b46264d88b6ce11a0e945038b"
         static let clientSecret = "81da385674a34ce895b92ac1b2612d6e"
         static let tokenAPIURL = "https://accounts.spotify.com/api/token"
+        static let redirectURI = "https://www.google.com"
+        static let scope = "user-read-private%20playlist-modify-private%20playlist-read-private%20playlist-modify-public%20user-library-modify%20user-read-email"
     }
     
     public var signInURL: URL? {
         let base = "https://accounts.spotify.com/authorize"
-        let redirectURI = "https://www.google.com"
-        let scope = "user-read-private"
-        let string = "\(base)?response_type=code&client_id=\(Constants.clientID)&scope=\(scope)&redirect_uri=\(redirectURI)&show_dialog=TRUE"
+        
+        let string = "\(base)?response_type=code&client_id=\(Constants.clientID)&scope=\(Constants.scope)&redirect_uri=\(Constants.redirectURI)&show_dialog=TRUE"
         
         return URL(string: string )
     }
@@ -64,7 +65,7 @@ final class AuthManager {
             URLQueryItem(name: "code",
                          value: code),
             URLQueryItem(name: "redirect_uri",
-                         value: "https://www.google.com")
+                         value: Constants.redirectURI )
         ]
         
         var request = URLRequest(url: url)
@@ -91,7 +92,6 @@ final class AuthManager {
             do {
                 let dataString = String(decoding: data, as: UTF8.self)
                 print(dataString)
-                let json = try JSONSerialization.jsonObject(with: data)
                 let result = try JSONDecoder().decode(AuthResponse.self, from: data)
                 self?.cacheToken(result: result)
                 completion(true)
@@ -107,19 +107,69 @@ final class AuthManager {
     
     private func cacheToken(result: AuthResponse) {
         UserDefaults.standard.setValue(result.accessToken, forKey: "accessToken")
-        UserDefaults.standard.setValue(result.refreshToken, forKey: "refreshToken")
+        if let refreshToken = result.refreshToken {
+            UserDefaults.standard.setValue(refreshToken, forKey: "refreshToken")
+        }
         UserDefaults.standard.setValue(Date().addingTimeInterval(TimeInterval(result.expirationTime)), forKey: "expirationDate")
         
     }
     
-    private func refreshAccessToken(completion: @escaping (Bool) -> Void ){
+    public func refreshAccessToken(completion: @escaping (Bool) -> Void ){
         guard shouldRefreshToken else {
             completion(true)
             return
         }
+        
         guard let refreshToken = self.refreshToken else {
             return
         }
+        
+        guard let url = URL(string: Constants.tokenAPIURL) else { return }
+
+        var components = URLComponents()
+        components.queryItems = [
+            URLQueryItem(name: "grant_type",
+                         value: "refresh_token"),
+            URLQueryItem(name: "refresh_token",
+                         value: refreshToken)
+        ]
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+        request.httpBody = components.query?.data(using: .utf8)
+        
+        let basicToken = Constants.clientID+":"+Constants.clientSecret
+        let dataForEncoding = basicToken.data(using: .utf8)
+        guard let base64String = dataForEncoding?.base64EncodedString() else {
+            print("Failed to encode to base64")
+            completion(false)
+            return
+        }
+        
+        request.setValue("Basic \(base64String)", forHTTPHeaderField: "Authorization")
+        
+        let task = URLSession.shared.dataTask(with: request) { [weak self] data, _, error in
+            guard let data = data, error == nil else {
+                completion(false)
+                return
+            }
+            
+            do {
+                let dataString = String(decoding: data, as: UTF8.self)
+                print(dataString)
+                let result = try JSONDecoder().decode(AuthResponse.self, from: data)
+                print("Succesfully refreshed token")
+                self?.cacheToken(result: result)
+                completion(true)
+            } catch {
+                print("Failed")
+                print(error)
+                completion(false)
+            }
+            
+        }
+        task.resume()
     }
 }
 
